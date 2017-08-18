67ee2e9ca3114189ea16cf737dd0defd8e4d9e38
Steve Haslam <araqnid@googlemail.com>
2017-08-18 21:50:50 +0100

    Continue to provide non-inline PositionCodec factories for pure Java clients

--- src/main/kotlin/org/araqnid/eventstore/Position.kt
+++ src/main/kotlin/org/araqnid/eventstore/Position.kt
@@ -8,6 +8,19 @@ interface PositionCodec {
     fun comparePositions(left: Position, right: Position): Int
 }
 
+fun <T> positionCodecOfComparable(clazz: Class<T>, encoder: (T) -> String, decoder: (String) -> T): PositionCodec where T : Position, T: Comparable<T> {
+    return object : PositionCodec {
+        override fun encode(position: Position): String = encoder(clazz.cast(position))
+        override fun decode(encoded: String): Position = decoder(encoded)
+
+        override fun comparePositions(left: Position, right: Position): Int {
+            val leftT = clazz.cast(left)
+            val rightT = clazz.cast(right)
+            return leftT.compareTo(rightT)
+        }
+    }
+}
+
 inline fun <reified T> positionCodecOfComparable(crossinline encoder: (T) -> String, crossinline decoder: (String) -> T): PositionCodec where T : Position, T: Comparable<T> {
     return object : PositionCodec {
         override fun encode(position: Position): String = encoder(T::class.java.cast(position))
@@ -17,6 +30,19 @@ inline fun <reified T> positionCodecOfComparable(crossinline encoder: (T) -> Str
             val leftT = T::class.java.cast(left)
             val rightT = T::class.java.cast(right)
             return leftT.compareTo(rightT)
+        }
+    }
+}
+
+fun <T : Position> positionCodecFromComparator(clazz: Class<T>, encoder: (T) -> String, decoder: (String) -> T, comparator: Comparator<T>): PositionCodec {
+    return object : PositionCodec {
+        override fun encode(position: Position): String = encoder(clazz.cast(position))
+        override fun decode(encoded: String): Position = decoder(encoded)
+
+        override fun comparePositions(left: Position, right: Position): Int {
+            val leftT = clazz.cast(left)
+            val rightT = clazz.cast(right)
+            return comparator.compare(leftT, rightT)
         }
     }
 }
