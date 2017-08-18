cfcd5d80dda561dbf1f00b3a9b9a9c8c38eaad58
Steve Haslam <araqnid@googlemail.com>
2017-08-18 21:12:54 +0100

    Use kotlin.streams.toList from stdlib-jre8

--- src/main/kotlin/org/araqnid/eventstore/StreamExtensions.kt
+++ src/main/kotlin/org/araqnid/eventstore/StreamExtensions.kt
@@ -2,14 +2,12 @@ package org.araqnid.eventstore
 
 import java.util.Objects
 import java.util.stream.Collector
-import java.util.stream.Collectors
 import java.util.stream.Stream
+import kotlin.streams.toList
 
 internal fun <T, R> Stream<T>.collectAndClose(collector: Collector<in T, *, out R>): R = use { it.collect(collector) }
-internal fun <T> Stream<T>.toListAndClose(): List<T> = collectAndClose(Collectors.toList())
+internal fun <T> Stream<T>.toListAndClose(): List<T> = use { it.toList() }
 internal fun <T> Stream<T>.forEachOrderedAndClose(action: (T) -> Unit) = use { it.forEachOrdered(action) }
-internal fun <T> Stream<T>.forEachAndClose(action: (T) -> Unit) = use { it.forEach(action) }
-internal fun <T> Stream<T>.findFirstAndClose(): T? = use { it.findFirst().orElse(null) }
 internal fun <T> Stream<T?>.filterNotNull(): Stream<T> {
     @Suppress("UNCHECKED_CAST")
     return this.filter(Objects::nonNull) as Stream<T>
