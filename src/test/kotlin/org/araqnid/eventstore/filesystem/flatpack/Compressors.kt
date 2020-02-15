ccf4c22e9204baec8a6df74d1d491bca87dec9b9
Steve Haslam <araqnid@googlemail.com>
2020-02-15 23:05:37 +0000

    Tidy up, using 'return when' and expression bodies

--- src/test/kotlin/org/araqnid/eventstore/filesystem/flatpack/Compressors.kt
+++ src/test/kotlin/org/araqnid/eventstore/filesystem/flatpack/Compressors.kt
@@ -13,74 +13,50 @@ import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
 
 fun uncompress(source: ByteSource, filename: String): ByteSource {
-    if (filename.endsWith(".gz")) {
-        return withGunzip(source)
-    } else if (filename.endsWith(".xz")) {
-        return withUnXZ(source)
-    } else {
-        throw IllegalArgumentException(filename)
+    return when {
+        filename.endsWith(".gz") -> withGunzip(source)
+        filename.endsWith(".xz") -> withUnXZ(source)
+        else -> throw IllegalArgumentException(filename)
     }
 }
 
 fun compress(sink: ByteSink, filename: String): ByteSink {
-    if (filename.endsWith(".gz")) {
-        return withGzip(sink)
-    } else if (filename.endsWith(".xz")) {
-        return withXZ(sink)
-    } else {
-        throw IllegalArgumentException(filename)
+    return when {
+        filename.endsWith(".gz") -> withGzip(sink)
+        filename.endsWith(".xz") -> withXZ(sink)
+        else -> throw IllegalArgumentException(filename)
     }
 }
 
-fun withGzip(underlying: ByteSink): ByteSink {
-    return GzipByteSink(underlying)
-}
+fun withGzip(underlying: ByteSink): ByteSink = GzipByteSink(underlying)
 
-fun withXZ(underlying: ByteSink): ByteSink {
-    return withXZ(underlying, LZMA2Options())
-}
+fun withXZ(underlying: ByteSink): ByteSink = withXZ(underlying, LZMA2Options())
 
 @Throws(UnsupportedOptionsException::class)
-fun withXZ(underlying: ByteSink, preset: Int): ByteSink {
-    return withXZ(underlying, LZMA2Options(preset))
-}
+fun withXZ(underlying: ByteSink, preset: Int): ByteSink = withXZ(underlying, LZMA2Options(preset))
 
-fun withXZ(underlying: ByteSink, options: LZMA2Options): ByteSink {
-    return XZByteSink(underlying, options)
-}
+fun withXZ(underlying: ByteSink, options: LZMA2Options): ByteSink = XZByteSink(underlying, options)
 
-fun withGunzip(underlying: ByteSource): ByteSource {
-    return GunzipByteSource(underlying)
-}
+fun withGunzip(underlying: ByteSource): ByteSource = GunzipByteSource(underlying)
 
-fun withUnXZ(underlying: ByteSource): ByteSource {
-    return UnXZByteSource(underlying)
-}
+fun withUnXZ(underlying: ByteSource): ByteSource = UnXZByteSource(underlying)
 
 private class GzipByteSink(private val underlying: ByteSink) : ByteSink() {
     @Throws(IOException::class)
-    override fun openStream(): OutputStream {
-        return GZIPOutputStream(underlying.openStream())
-    }
+    override fun openStream(): OutputStream = GZIPOutputStream(underlying.openStream())
 }
 
 private class GunzipByteSource(private val underlying: ByteSource) : ByteSource() {
     @Throws(IOException::class)
-    override fun openStream(): InputStream {
-        return GZIPInputStream(underlying.openStream())
-    }
+    override fun openStream(): InputStream = GZIPInputStream(underlying.openStream())
 }
 
 private class XZByteSink(private val underlying: ByteSink, private val options: LZMA2Options) : ByteSink() {
     @Throws(IOException::class)
-    override fun openStream(): OutputStream {
-        return XZOutputStream(underlying.openBufferedStream(), options)
-    }
+    override fun openStream(): OutputStream = XZOutputStream(underlying.openBufferedStream(), options)
 }
 
 private class UnXZByteSource(private val underlying: ByteSource) : ByteSource() {
     @Throws(IOException::class)
-    override fun openStream(): InputStream {
-        return XZInputStream(underlying.openBufferedStream())
-    }
+    override fun openStream(): InputStream = XZInputStream(underlying.openBufferedStream())
 }
