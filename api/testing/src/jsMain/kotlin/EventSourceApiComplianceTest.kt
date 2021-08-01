2dfa6983c226c402785afa95bc091c04b46d2662
Steve Haslam <araqnid@gmail.com>
2021-08-01 02:27:24 +0100

    Wrap up compliance tests the same way and avoid GlobalScope

--- api/testing/src/jsMain/kotlin/EventSourceApiComplianceTest.kt
+++ api/testing/src/jsMain/kotlin/EventSourceApiComplianceTest.kt
@@ -1,8 +1,10 @@
 package org.araqnid.eventstore.testing
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.promise
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.araqnid.eventstore.EventSource
+import kotlin.js.Promise
 import kotlin.test.Test
 
 actual abstract class EventSourceApiComplianceTest {
@@ -10,47 +12,64 @@ actual abstract class EventSourceApiComplianceTest {
 
     private val impls by lazy { ComplianceTestImplementations(eventSource) }
 
-    @Test fun read_events_written_to_stream() = GlobalScope.promise {
-        impls.read_events_written_to_stream()
-    }
+    @Test
+    fun read_events_written_to_stream() =
+        runTest(impls::read_events_written_to_stream)
 
-    @Test fun read_and_write_to_streams_independently() = GlobalScope.promise {
-        impls.read_and_write_to_streams_independently()
-    }
+    @Test
+    fun read_and_write_to_streams_independently() =
+        runTest(impls::read_and_write_to_streams_independently)
 
-    @Test fun write_events_specifying_expected_version_number() = GlobalScope.promise {
-        impls.write_events_specifying_expected_version_number()
-    }
+    @Test
+    fun write_events_specifying_expected_version_number() =
+        runTest(impls::write_events_specifying_expected_version_number)
 
-    @Test fun write_events_specifying_expected_empty_version_number() = GlobalScope.promise {
-        impls.write_events_specifying_expected_empty_version_number()
-    }
+    @Test
+    fun write_events_specifying_expected_empty_version_number() =
+        runTest(impls::write_events_specifying_expected_empty_version_number)
 
-    @Test fun fails_if_expected_event_number_not_satisfied_yet() = GlobalScope.promise {
-        impls.fails_if_expected_event_number_not_satisfied_yet()
-    }
+    @Test
+    fun fails_if_expected_event_number_not_satisfied_yet() =
+        runTest(impls::fails_if_expected_event_number_not_satisfied_yet)
 
-    @Test fun fails_if_expected_event_number_already_passed() = GlobalScope.promise {
-        impls.fails_if_expected_event_number_already_passed()
-    }
+    @Test
+    fun fails_if_expected_event_number_already_passed() =
+        runTest(impls::fails_if_expected_event_number_already_passed)
 
-    @Test fun read_stream_after_specific_event_number() = GlobalScope.promise {
-        impls.read_stream_after_specific_event_number()
-    }
+    @Test
+    fun read_stream_after_specific_event_number() =
+        runTest(impls::read_stream_after_specific_event_number)
 
-    @Test fun read_empty_after_end_of_stream() = GlobalScope.promise {
-        impls.read_empty_after_end_of_stream()
-    }
+    @Test
+    fun read_empty_after_end_of_stream() =
+        runTest(impls::read_empty_after_end_of_stream)
 
-    @Test fun read_all_events() = GlobalScope.promise {
-        impls.read_all_events()
-    }
+    @Test
+    fun read_all_events() =
+        runTest(impls::read_all_events)
 
-    @Test fun read_all_events_from_position() = GlobalScope.promise {
-        impls.read_all_events_from_position()
-    }
+    @Test
+    fun read_all_events_from_position() =
+        runTest(impls::read_all_events_from_position)
 
-    @Test fun read_empty_after_end_of_store() = GlobalScope.promise {
-        impls.read_empty_after_end_of_store()
+    @Test
+    fun read_empty_after_end_of_store() =
+        runTest(impls::read_empty_after_end_of_store)
+}
+
+private fun runTest(testBody: suspend () -> Unit): Promise<Unit> {
+    return Promise { resolve, reject ->
+        val job = Job()
+        val scope = CoroutineScope(job)
+        scope.launch {
+            testBody()
+        }
+        job.invokeOnCompletion { ex: Throwable? ->
+            if (ex == null)
+                resolve(Unit)
+            else
+                reject(ex)
+        }
+        job.complete()
     }
 }
--- api/testing/src/jvmMain/kotlin/EventSourceApiComplianceTest.kt
+++ api/testing/src/jvmMain/kotlin/EventSourceApiComplianceTest.kt
@@ -1,3 +1,4 @@
+@file:JvmName("EventSourceApiComplianceTestJVM")
 package org.araqnid.eventstore.testing
 
 import kotlinx.coroutines.runBlocking
@@ -9,47 +10,53 @@ actual abstract class EventSourceApiComplianceTest {
 
     private val impls by lazy { ComplianceTestImplementations(eventSource) }
 
-    @Test fun read_events_written_to_stream() = runBlocking {
-        impls.read_events_written_to_stream()
-    }
+    @Test
+    fun read_events_written_to_stream() =
+        runTest(impls::read_events_written_to_stream)
 
-    @Test fun read_and_write_to_streams_independently() = runBlocking {
-        impls.read_and_write_to_streams_independently()
-    }
+    @Test
+    fun read_and_write_to_streams_independently() =
+        runTest(impls::read_and_write_to_streams_independently)
 
-    @Test fun write_events_specifying_expected_version_number() = runBlocking {
-        impls.write_events_specifying_expected_version_number()
-    }
+    @Test
+    fun write_events_specifying_expected_version_number() =
+        runTest(impls::write_events_specifying_expected_version_number)
 
-    @Test fun write_events_specifying_expected_empty_version_number() = runBlocking {
-        impls.write_events_specifying_expected_empty_version_number()
-    }
+    @Test
+    fun write_events_specifying_expected_empty_version_number() =
+        runTest(impls::write_events_specifying_expected_empty_version_number)
 
-    @Test fun fails_if_expected_event_number_not_satisfied_yet() = runBlocking {
-        impls.fails_if_expected_event_number_not_satisfied_yet()
-    }
+    @Test
+    fun fails_if_expected_event_number_not_satisfied_yet() =
+        runTest(impls::fails_if_expected_event_number_not_satisfied_yet)
 
-    @Test fun fails_if_expected_event_number_already_passed() = runBlocking {
-        impls.fails_if_expected_event_number_already_passed()
-    }
+    @Test
+    fun fails_if_expected_event_number_already_passed() =
+        runTest(impls::fails_if_expected_event_number_already_passed)
 
-    @Test fun read_stream_after_specific_event_number() = runBlocking {
-        impls.read_stream_after_specific_event_number()
-    }
+    @Test
+    fun read_stream_after_specific_event_number() =
+        runTest(impls::read_stream_after_specific_event_number)
 
-    @Test fun read_empty_after_end_of_stream() = runBlocking {
-        impls.read_empty_after_end_of_stream()
-    }
+    @Test
+    fun read_empty_after_end_of_stream() =
+        runTest(impls::read_empty_after_end_of_stream)
 
-    @Test fun read_all_events() = runBlocking {
-        impls.read_all_events()
-    }
+    @Test
+    fun read_all_events() =
+        runTest(impls::read_all_events)
 
-    @Test fun read_all_events_from_position() = runBlocking {
-        impls.read_all_events_from_position()
-    }
+    @Test
+    fun read_all_events_from_position() =
+        runTest(impls::read_all_events_from_position)
 
-    @Test fun read_empty_after_end_of_store() = runBlocking {
-        impls.read_empty_after_end_of_store()
+    @Test
+    fun read_empty_after_end_of_store() =
+        runTest(impls::read_empty_after_end_of_store)
+}
+
+private fun runTest(testBody: suspend () -> Unit) {
+    runBlocking {
+        testBody()
     }
 }
