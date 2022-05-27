ab993ebfc180d808b7d7539188702a6876114d0e
Steve Haslam <araqnid@gmail.com>
2022-05-27 23:48:03 +0100

    Update kotlinx-datetime

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -16,7 +16,7 @@ java {
 dependencies {
     api(kotlin("stdlib-jdk8"))
     api(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.2"))
-    api("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
+    api("org.jetbrains.kotlinx:kotlinx-datetime:0.3.3")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
     api("com.google.guava:guava:30.1.1-jre")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.2")
